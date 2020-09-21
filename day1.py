da44f5535cb39245ae80113033af279967b46027
Chelsea <chelslaguma@gmail.com>
2020-09-21 11:02:51 +0200

    First days work

---
+++ day1.py
@@ -0,0 +1,10 @@
+print("Hello world")
+
+class_name = " class 2"
+
+print("I was coded in" + class_name)
+
+class_students = " 20 "
+
+print("There are " + class_students + "students in " + class_name)
+
